6674b2ae510f2701c54fd018a134c4d57a4cbeae
Max-Andre' Zils <zilsmc@gmail.com>
2018-08-19 00:32:02 +0200

    added notes

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # JS-BOILERPLATE
 ---
+
+## Description
+
+## How to Run the Application
+The following two commands need to be run from a terminal to initialize and run the project:
+  1. ``` npm i ```
+  2. ``` npm start ```
+
+## Packages
+  1. express - used for the server.js and routing
+  2. eslint is used for linting and includes the following
+
+    2.1. eslint-config-airbnb-base  -- use of airbnb linting standards.
+
+    2.2. eslint-plugin-import -- enables eslintrc to import the airbnb standard
+
+  3.   
+## TO DO
+- add webpack
+- add sass to the project
+- add routing for Packages
+- add a testing framework
+- add a templating engine
+-
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const path = require('path');
 const open = require('open');
+const chalk = require('chalk');
 
 const app = express();
 const htmlDirectory = '/src/views/';
 const port = 3000;
 
+/* TODO: add static file reading */
+app.use(express.static('/static', path.join(__dirname, '/src')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, `${htmlDirectory}index.html`));
 });
 /* eslint-disable no-console */
+
 app.listen(port, (err) => {
   if (err) {
-    console.log(err);
+    console.log(chalk.red(err));
   } else {
-    console.log(`Listening on port ${port}`);
+    console.log(chalk.green(`Listening on port ${port}`));
     open(`http://localhost:${port}`);
   }
 });
